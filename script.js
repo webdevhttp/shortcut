b0126e7307a3d569c531a75f67102048fadc5071
Tasha SEii <liamluke2012@icloud.com>
2024-08-08 13:13:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var price=5;
+var quantity=14;
+var total=price*quantity;
+var elCost=document.getElementById('cost');
+elCost.textContent= "$" +total;
+
+
+
+var price,quantity,total;
+price=5;
+quantity=14
+total=price*quantity;
+var elCostx=document.getElementById('cost1');
+elCostx.textContent= "$"+total; 
+
+var price=5;var quantity=14;var total=price*quantity;
+
+var elCost3=document.getElementById('cost3');
+elCost3.textContent='$'+total;
+
+
